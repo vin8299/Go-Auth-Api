7cf0480ffcc38e47c2dc2714c9c86d08e23487b8
Vineet Chauhan <vineet.chauhan@indiamart.com>
2022-03-27 20:33:23 +0530

    documentation added

--- routes/routes.go
+++ routes/routes.go
@@ -7,6 +7,6 @@ import(
 func Setup(app *fiber.App){
 	app.Post("/auth/register", controllers.Register)
 	app.Post("/auth/login",controllers.Login)
-	app.Get("/auth/userdetail",controllers.UserDetail)
+	app.Get("/user/userdetail",controllers.UserDetail)
 	app.Get("/auth/logout",controllers.Logout)
 }
