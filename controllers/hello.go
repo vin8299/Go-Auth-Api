43edf8a289a4defefdd7094708c1470cd52dace1
sonuskidos <sonu@skidos.com>
2022-04-04 13:43:12 +0530

    Fixed compilation error from Hello API

--- controllers/hello.go
+++ controllers/hello.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Hello(c *fiber.Ctx)(error) {
-	ouput := make(map[string]string)
+	output := make(map[string]string)
         output["msg"] = "Hello World"
 	return c.JSON(output)
 }
